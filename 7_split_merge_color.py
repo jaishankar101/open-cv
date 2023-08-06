9332331a1ed0050244f36fed7114ba9a0c9d1084
Jai Shankar K S <jai.shankar@hingehealth.com>
2023-08-06 20:06:26 +0530

    #7:final copy

--- 7_split_merge_color.py
+++ 7_split_merge_color.py
@@ -9,16 +9,29 @@ blue,green,red=cv.split(img)
 # cv.imshow("red",red)
 
 
-# merge the individual colors
-mergerd_img=cv.merge([blue,green,red])
-cv.imshow("merges image",mergerd_img)
+# # merge the individual colors
+# mergerd_img=cv.merge([blue,green,red])
+# cv.imshow("merges image",mergerd_img)
 
 
-import numpy as np
 
-blank=np.zeros(img.shape[2:],dtype='uint8')
+# # to split the image as red,blue and greencomponents
+# import numpy as np
 
- 
+# blank=np.zeros(img.shape[:2],dtype='uint8')
+
+# blue_img=cv.merge([blue,blank,blank])
+# green_img=cv.merge([blank,green,blank])
+# red_img=cv.merge([blank,blank,red])
+
+# cv.imshow("blue image",blue_img)
+# cv.imshow("green image",green_img)
+# cv.imshow("red image",red_img)
+
+
+
+
+
 
 # cv.imshow("dog",img)
 cv.waitKey(0)
