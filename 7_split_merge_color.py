0d3c0db8969ab273b1b17dbffd6e3bf99ad43418
JaiShankar <jaishankar825055@gmail.com>
2023-08-06 19:47:46 +0530

    changes to #7

--- 7_split_merge_color.py
+++ 7_split_merge_color.py
@@ -2,16 +2,21 @@ import cv2 as cv
 
 img = cv.imread("images/dog.jpeg")
 
-# # splits the image into B,G and R
-# blue,green,red=cv.split(img) 
+# splits the image into B,G and R
+blue,green,red=cv.split(img) 
 # cv.imshow("blue",blue)
 # cv.imshow("green",green)
 # cv.imshow("red",red)
 
 
-# mergge the individual colors
-mergerd_img=cv.merge()
+# merge the individual colors
+mergerd_img=cv.merge([blue,green,red])
+cv.imshow("merges image",mergerd_img)
 
+
+import numpy as np
+
+blank=np.zeros(img.shape[2:],dtype='uint8')
 
  
 
